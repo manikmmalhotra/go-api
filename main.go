65df3b97b3b3dae185aa0c5561df15dd7bcf5438
manikmmalhotra <manikmalhotra2113@gmail.com>
2023-06-01 13:35:49 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"go-api/database"
+	"go-api/router"
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/joho/godotenv"
+)
+
+func getenv(key, fallback string) string {
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		return fallback
+	}
+	return value
+}
+
+func main() {
+	godotenv.Load()
+	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*", // comma string format e.g. "localhost, nikschaefer.tech"
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
+
+	database.ConnectDB()
+
+	router.Initialize(app)
+	log.Fatal(app.Listen(":" + getenv("PORT", "3000")))
+}
